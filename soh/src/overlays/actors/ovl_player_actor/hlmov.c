6aab3f2c24887c4cb7998f0c05314ed6417a06a2
Bryan Haley <bryanhaley.professional@gmail.com>
2025-09-09 16:03:20 -0700

    Switch back to line test for walls and ceilings

--- soh/src/overlays/actors/ovl_player_actor/hlmov.c
+++ soh/src/overlays/actors/ovl_player_actor/hlmov.c
@@ -197,6 +197,73 @@ pmtrace_t PM_PlayerTrace(vec3_t start, vec3_t end, int traceFlags, int ignore_pe
     vec3_t hitPos;
     CollisionPoly* poly = NULL;
     int bgId = -1;
+
+    vec3_t start_oot = v_goldsrc_to_oot(start);
+    vec3_t end_oot = v_goldsrc_to_oot(end);
+
+    // Perform the line trace (check wall, floor, and ceiling)
+    int hit = BgCheck_EntityLineTest1(&play->colCtx, &start_oot, &end_oot, &hitPos, &poly, 1, 0, 1, 1, &bgId);
+
+    vec3_t hitPos_gs = v_oot_to_goldsrc(hitPos);
+
+	/*vec3_t zero = {0,0,0};
+	EffectSsKiraKira_SpawnSmallYellow(play, &end_oot, &zero, &zero);*/
+
+    if (hit && poly != NULL) {
+        tr.fraction = sqrtf(
+            (hitPos_gs.x - start.x) * (hitPos_gs.x - start.x) +
+            (hitPos_gs.y - start.y) * (hitPos_gs.y - start.y) +
+            (hitPos_gs.z - start.z) * (hitPos_gs.z - start.z)
+        ) / sqrtf(
+            (end.x - start.x) * (end.x - start.x) +
+            (end.y - start.y) * (end.y - start.y) +
+            (end.z - start.z) * (end.z - start.z)
+        );
+        tr.endpos.x = hitPos_gs.x;
+        tr.endpos.y = hitPos_gs.y;
+        tr.endpos.z = hitPos_gs.z;
+
+        // Fill in the plane normal from the poly
+        tr.plane.normal.x = COLPOLY_GET_NORMAL(poly->normal.x);
+        tr.plane.normal.y = COLPOLY_GET_NORMAL(poly->normal.y);
+        tr.plane.normal.z = COLPOLY_GET_NORMAL(poly->normal.z);
+
+        tr.plane.normal = v_oot_to_goldsrc(tr.plane.normal);
+        VectorNormalize(&tr.plane.normal);
+
+        // Check if start or end is inside solid
+        int startSolid = BgCheck_PosInStaticBoundingBox(&play->colCtx, &start_oot);
+        int endSolid = BgCheck_PosInStaticBoundingBox(&play->colCtx, &end_oot);
+        tr.startsolid = (startSolid != 0) ? 1 : 0;
+        tr.allsolid = (tr.startsolid && (endSolid != 0)) ? 1 : 0;
+        //tr.startsolid = 0;
+        //tr.allsolid = 0;
+
+        tr.ent = bgId;     // Set to bgId or 0 for world
+    } else {
+        // No hit, fraction is 1, endpos is end
+        tr.fraction = 1.0f;
+        tr.endpos = end;
+        tr.plane.normal.x = 0.0f;
+        tr.plane.normal.y = 0.0f;
+        tr.plane.normal.z = 1.0f;
+        tr.startsolid = 0;
+        tr.allsolid = 0;
+        tr.ent = 0;
+		tr.inopen = 1;
+    }
+
+    return tr;
+}
+
+pmtrace_t PM_PlayerTrace_Custom(vec3_t start, vec3_t end, int traceFlags, int ignore_pe)
+{
+    pmtrace_t tr;
+    memset(&tr, 0, sizeof(pmtrace_t));
+
+    vec3_t hitPos;
+    CollisionPoly* poly = NULL;
+    int bgId = -1;
     int startSolid = false;
     int endSolid = false;
     vec3_t start_oot = v_goldsrc_to_oot(start);
@@ -319,6 +386,62 @@ qboolean PM_CheckWater ()
 	return pmove->waterlevel > 1;
 }
 
+/*void PM_CatagorizePosition (void)
+{
+	vec3_t		point;
+	pmtrace_t		tr;
+
+// if the player hull point one unit down is solid, the player
+// is on ground
+
+// see if standing on something solid	
+
+	// Doing this before we move may introduce a potential latency in water detection, but
+	// doing it after can get us stuck on the bottom in water if the amount we move up
+	// is less than the 1 pixel 'threshold' we're about to snap to.	Also, we'll call
+	// this several times per frame, so we really need to avoid sticking to the bottom of
+	// water on each call, and the converse case will correct itself if called twice.
+	PM_CheckWater();
+
+	vec3_t origin = pmove->origin;
+	origin.z += 1;
+	point.x = pmove->origin.x;
+	point.y = pmove->origin.y;
+	point.z = pmove->origin.z - 1;
+
+	if (pmove->velocity.z > 180)   // Shooting up really fast.  Definitely not on ground.
+	{
+		pmove->onground = -1;
+	}
+	else
+	{
+		// Try and move down.
+		tr = pmove->PM_PlayerTrace (origin, point, PM_NORMAL, -1 );
+
+		if (tr.ent == player->actor.id)
+		{	// Don't clip against yourself.
+			pmove->onground = -1;
+			return;
+		}
+
+		// If we hit a steep plane, we are not on ground
+		if (tr.plane.normal.z < 0.7)
+			pmove->onground = -1;	// too steep
+		else
+			pmove->onground = tr.ent;  // Otherwise, point to index of ent under us.
+
+		// If we are on something...
+		if (pmove->onground != -1)
+		{
+			// Then we are not in water jump sequence
+			pmove->waterjumptime = 0;
+			// If we could make the move, drop us down that 1 pixel
+			if (pmove->waterlevel < 2 && !tr.startsolid && !tr.allsolid)
+				VectorCopy (tr.endpos, pmove->origin);
+		}
+	}
+}*/
+
 void PM_CatagorizePosition (void)
 {
 	vec3_t		point;
@@ -350,7 +473,7 @@ void PM_CatagorizePosition (void)
 	else
 	{
 		// Try and move down.
-		tr = pmove->PM_PlayerTrace (origin, point, PM_NORMAL, -1 );
+		tr = PM_PlayerTrace_Custom (origin, point, PM_NORMAL, -1 );
 
 		if (tr.fraction < 1.0f)
 		{
@@ -372,49 +495,6 @@ void PM_CatagorizePosition (void)
 		}
 	}
 }
-
-/*void PM_CatagorizePosition(void) {
-    if (pmove->velocity.z > 180)   // Shooting up really fast.  Definitely not on ground.
-	{
-		pmove->onground = -1;
-	}
-	else
-	{
-		CollisionPoly *poly = NULL;
-		int bgId = -1;
-		vec3_t checkFrom = v_goldsrc_to_oot(pmove->origin);
-		checkFrom.y += 60 * GOLDSRC_UNIT_SCALE;
-		float floorHeight = BgCheck_EntityRaycastFloor5(play, &play->colCtx, &poly, &bgId, &player->actor, &checkFrom);
-		float floorHeightDiff = floorHeight - v_goldsrc_to_oot(pmove->origin).y;
-
-		printf("Floor height: %f, diff: %f\n", floorHeight, floorHeightDiff);
-
-		if (floorHeight != BGCHECK_Y_MIN && floorHeightDiff >= 0.0f && floorHeightDiff <= sv_stepsize) 
-		{
-			vec3_t new_origin = v_goldsrc_to_oot(pmove->origin);
-			new_origin.y = floorHeight;
-			pmove->origin = v_oot_to_goldsrc(new_origin);
-			
-			vec3_t plane_normal;
-			plane_normal.x = COLPOLY_GET_NORMAL(poly->normal.x);
-			plane_normal.y = COLPOLY_GET_NORMAL(poly->normal.y);
-			plane_normal.z = COLPOLY_GET_NORMAL(poly->normal.z);
-
-			plane_normal = v_oot_to_goldsrc(plane_normal);
-			VectorNormalize(&plane_normal);
-
-			if (plane_normal.z < 0.7f) {
-				pmove->onground = -1;	// too steep
-			} else {
-				pmove->onground = bgId;
-			}
-		}
-		else
-		{
-			pmove->onground = -1;
-		}
-	}
-}*/
 
 /*void PM_CatagorizePosition (void)
 {
@@ -906,7 +986,7 @@ int PM_FlyMove (void)
 	int			numplanes;
 	vec3_t		planes[MAX_CLIP_PLANES];
 	vec3_t		primal_velocity, original_velocity;
-	vec3_t      new_velocity = {0,0,0};
+	vec3_t      new_velocity;
 	int			i, j;
 	pmtrace_t	trace;
 	vec3_t		end;
@@ -930,19 +1010,17 @@ int PM_FlyMove (void)
 
 		// Assume we can move all the way from the current origin to the
 		//  end point.
-		vec3_t origin = pmove->origin;
-		origin.z += 0.01f; // Bump the origin up slightly so it doesn't look like we're "inside" the ground
-        end.x = origin.x + time_left * pmove->velocity.x;
-        end.y = origin.y + time_left * pmove->velocity.y;
-        end.z = origin.z + time_left * pmove->velocity.z;
+        end.x = pmove->origin.x + time_left * pmove->velocity.x;
+        end.y = pmove->origin.y + time_left * pmove->velocity.y;
+        end.z = pmove->origin.z + time_left * pmove->velocity.z;
 
 		/*VectorCopy (end, pmove->origin);
 		return; // testing*/
 
 		// See if we can make it from origin to end point.
-        trace = pmove->PM_PlayerTrace(origin, end, PM_NORMAL, -1);
+		trace = pmove->PM_PlayerTrace (pmove->origin, end, PM_NORMAL, -1 );
 
-#if 1 // Sneaky: slip in slopefix here. See if speedrunners notice. /// TODO
+#if 0 // Sneaky: slip in slopefix here. See if speedrunners notice. /// TODO
 		// Check if we are stuck on the surface (HACKHACK: this solves precision error in the engine for small movements)
 		if (trace.fraction == 0.0)
 		{
@@ -950,7 +1028,7 @@ int PM_FlyMove (void)
             end.x += trace.plane.normal.x * 0.001;
             end.y += trace.plane.normal.y * 0.001;
             end.z += trace.plane.normal.z * 0.001;
-            trace = pmove->PM_PlayerTrace(origin, end, PM_NORMAL, -1);
+			trace = pmove->PM_PlayerTrace(pmove->origin, end, PM_NORMAL, -1);
 		}
 #endif
 
@@ -1296,6 +1374,7 @@ usedown:
 		VectorCopy (downvel, pmove->velocity);
 	} else // copy z value from slide move
 		pmove->velocity.z = downvel.z;
+
 }
 
 void PM_AirAccelerate (vec3_t wishdir, float wishspeed, float accel)
