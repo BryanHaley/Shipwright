82bc7eeeb53edbd6db8dd61cb532584f5dd1f7c4
Bryan Haley <bryanmhaley@gmail.com>
2025-09-09 22:32:33 -0700

    Update SohInputEditorWindow.cpp

--- soh/soh/Enhancements/controls/SohInputEditorWindow.cpp
+++ soh/soh/Enhancements/controls/SohInputEditorWindow.cpp
@@ -1360,19 +1360,19 @@ void SohInputEditorWindow::DrawCameraControlPanel() {
             .Color(THEME_COLOR)
             .Tooltip("Allows for using the mouse to control the camera (must enable Free Look), "
                      "aim with the shield, and perform quickspin attacks (quickly rotate the mouse then press B)")
-            .DefaultValue(1));
+            .DefaultValue(true));
     Ship::GuiWindow::BeginGroupPanel("Aiming/First-Person Camera", ImGui::GetContentRegionAvail());
     CVarCheckbox("Right Stick Aiming", CVAR_SETTING("Controls.RightStickAim"),
                  CheckboxOptions()
                      .Color(THEME_COLOR)
                      .Tooltip("Allows for aiming with the right stick in:\n-First-Person/C-Up view\n-Weapon Aiming")
-                     .DefaultValue(1));
+                     .DefaultValue(true));
     if (CVarGetInteger(CVAR_SETTING("Controls.RightStickAim"), 0)) {
         CVarCheckbox("Allow moving while in first-person mode", CVAR_SETTING("MoveInFirstPerson"),
                      CheckboxOptions()
                          .Color(THEME_COLOR)
                          .Tooltip("Changes the left stick to move the player while in first-person mode")
-                         .DefaultValue(1));
+                         .DefaultValue(true));
     }
     CVarCheckbox("Invert Aiming X Axis", CVAR_SETTING("Controls.InvertAimingXAxis"),
                  CheckboxOptions()
@@ -1396,9 +1396,9 @@ void SohInputEditorWindow::DrawCameraControlPanel() {
                  CheckboxOptions()
                      .Color(THEME_COLOR)
                      .Tooltip("Prevents the C-Up view from auto-centering, allowing for Gyro Aiming")
-                     .DefaultValue(1));
+                     .DefaultValue(true));
     if (CVarCheckbox("Enable Custom Aiming/First-Person sensitivity",
-                     CVAR_SETTING("FirstPersonCameraSensitivity.Enabled"), CheckboxOptions().Color(THEME_COLOR).DefaultValue(1))) {
+                     CVAR_SETTING("FirstPersonCameraSensitivity.Enabled"), CheckboxOptions().Color(THEME_COLOR).DefaultValue(true))) {
         if (!CVarGetInteger(CVAR_SETTING("FirstPersonCameraSensitivity.Enabled"), 0)) {
             CVarClear(CVAR_SETTING("FirstPersonCameraSensitivity.X"));
             CVarClear(CVAR_SETTING("FirstPersonCameraSensitivity.Y"));
